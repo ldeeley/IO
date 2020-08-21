df2a0eb8e5c74a4084b1a950f6bad4e0b35020ad
Lester Deeley <lester.deeley@yahoo.com>
2020-08-21 16:03:46 +0100

    Read 3 file names from the console : file1, file2, file3. Split file1 as follows...Write the first half of its bytes to file2 and the second half of its bytes to file3.

--- src/SplittingAFile.java
+++ src/SplittingAFile.java
@@ -1,6 +1,39 @@
+import java.io.*;
+
 public class SplittingAFile {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String sourceFile = "C:/Users/leste/IdeaProjects/IO/data.txt";
+        String splitFile1 = reader.readLine();
+        String splitFile2 = reader.readLine();
+
+        FileInputStream fileInputStream = new FileInputStream(sourceFile);
+        FileOutputStream fileOutputStream2 = new FileOutputStream(splitFile1);
+        FileOutputStream fileOutputStream3 = new FileOutputStream(splitFile2);
+
+        int sourceFileSize = fileInputStream.available();
+        int splitFile2Size = sourceFileSize/2;
+        int splitFile1Size = sourceFileSize-splitFile2Size;
+
+        int ch;
+
+        for (int i = 1; i<=splitFile1Size; i=i+1){
+            ch = fileInputStream.read();
+            fileOutputStream2.write(ch);
+        }
+        while (fileInputStream.available()>0){
+            ch = fileInputStream.read();
+            fileOutputStream3.write(ch);
+        }
+
+        fileInputStream.close();
+        fileOutputStream2.close();
+        fileOutputStream3.close();
+
+
 
     }
 
